8af51f9c543e39a82aa6b3e2cf26039d00da7f6f
guojc <2639880697@qq.com>
2024-05-25 18:09:32 +0800

    update

---
+++ 解决非SDK接口的限制失效.sh
@@ -0,0 +1,5 @@
+su
+settings delete global hidden_api_policy
+settings delete global hidden_api_policy_p_apps
+settings delete global hidden_api_policy_pre_p_apps
+settings delete global hidden_api_blacklist_exemptions
